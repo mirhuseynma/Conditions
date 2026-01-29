73375a0507d0cc34f072d7d20a5145630c038e6c
mirhuseynma <mirhuseynma@code.edu.az>
2026-01-29 23:26:25 +0400

    Conditions

---
+++ Conditions/Program.cs
@@ -0,0 +1,35 @@
+﻿// 1 - den 100 - e qeder edelerin cemini tapan algoritm
+using System;
+using System.Threading.Channels;
+int a = 0;
+int b = 100;
+int sum = 0;
+for (int i = a; i <= b; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
+
+
+// 1 - den 100 - e qeder edelerin cemini tapan algoritm
+int c = 0;
+int d = 100;
+int total = 0;
+while (c <= d)
+{
+    total += c;
+    c++;
+}
+Console.WriteLine(total);
+
+// 1 - den 100 - e qeder olan ededlerin cemi
+int g = 0;
+int h = 100;
+int tot = 0;
+do { 
+    tot += g;
+    g++;
+} while (g <= h);
+Console.WriteLine(tot);
+
+// 1 den 50 e qeder olan ededdleri yazdir
