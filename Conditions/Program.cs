9ec751af09867f441c639615ddec11ad26e6a484
mirhuseynma <mirhuseynma@code.edu.az>
2026-01-30 01:56:28 +0400

    ADDCOde

--- Conditions/Program.cs
+++ Conditions/Program.cs
@@ -1,4 +1,4 @@
-﻿// 1 - den 100 - e qeder olan ededlerin  cemini tapan algoritm
+﻿//1 - den 100 - e qeder olan ededlerin  cemini tapan algoritm
 using System;
 using System.Threading.Channels;
 int a = 0;
@@ -11,7 +11,7 @@ for (int i = a; i <= b; i++)
 Console.WriteLine(sum);
 
 
-// 1 - den 100 - e qeder olan ededlerin cemini tapan algoritm
+//1 - den 100 - e qeder olan ededlerin cemini tapan algoritm
 int c = 0;
 int d = 100;
 int total = 0;
@@ -22,12 +22,36 @@ while (c <= d)
 }
 Console.WriteLine(total);
 
-// 1 - den 100 - e qeder olan ededlerin cemini tapan alqoritm
+//1 - den 100 - e qeder olan ededlerin cemini tapan alqoritm
 int g = 0;
 int h = 100;
 int tot = 0;
-do { 
+do
+{
     tot += g;
     g++;
 } while (g <= h);
 Console.WriteLine(tot);
+
+// ededin sade ve ya murekkeb olmagini yoxlamag
+int number = 2;
+if ((number % 2 == 0 | number % 3 == 0) & number > 3)
+{
+    Console.WriteLine("Daxil etdiyiniz " + "'" + number + "'" + " ededi murekkebdir!");
+}
+else
+{
+    Console.WriteLine("Daxil etdiyiniz " + "'" + number + "'" + " ededi sadedir!");
+}
+
+
+// verilmis ededin mertebelerini ekrana cap etdirmek
+int value = 4545454;
+int count = 0;
+int cloneval = value;
+while(value > 0)
+{
+    value /= 10;
+    count++;
+}
+Console.WriteLine("Daxil etdiyiniz " + "'" + cloneval + "'"  + " ededi " + count  + " mertebeden ibaretdir");
--- Conditions/obj/Debug/net10.0/Conditions.AssemblyInfo.cs
+++ Conditions/obj/Debug/net10.0/Conditions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conditions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e6b941637470b612439776e647366a7b134cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conditions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conditions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
