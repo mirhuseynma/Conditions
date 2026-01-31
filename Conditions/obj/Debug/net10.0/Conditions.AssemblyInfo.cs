2263fbb58836874e51afcfc8b1b01adc94c8e8dc
mirhuseynma <mirhuseynma@code.edu.az>
2026-01-31 20:04:57 +0400

    changes

--- Conditions/obj/Debug/net10.0/Conditions.AssemblyInfo.cs
+++ Conditions/obj/Debug/net10.0/Conditions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conditions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c91c477006d5fcf0af713388b52cf43253051be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae891e142fdaa23dee325ba06c044e56d32d8289")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conditions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conditions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
